e777ba1e3653f498a839b2d00bcd8b1097e5fdaf
Oleg Suchkov <als.b.software@gmail.com>
2023-01-22 13:31:57 +0300

    Add Appointments controller

---
+++ app/controllers/api/v1/appointments_controller.rb
@@ -0,0 +1,50 @@
+class Api::V1::AppointmentsController < ApplicationController
+  before_action :set_appointment, only: %i[show update destroy]
+
+  def index
+    @appointments = Appointment.all
+
+    render json: @appointments
+  end
+
+  def show
+    render json: @appointment
+  end
+
+  def create
+    @appointment = Appointment.new(appointment_params)
+
+    if @appointment.save
+      render json: @appointment, status: :created
+    else
+      render json: @appointment.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @appointment.update(appointment_params)
+      render json: @appointment
+    else
+      render json: @appointment.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @appointment.destroy
+  end
+
+  private
+
+  def set_appointment
+    @appointment = Appointment.find(params[:id])
+  end
+
+  def appointment_params
+    date = convert_to_datetime(params.require(:appointment)[:date])
+    params.require(:appointment).permit(:service_provider_id, :user_id, :date).merge(date:)
+  end
+
+  def convert_to_datetime(date)
+    Time.at(date.to_i).utc if date.present?
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+namespace :api do
+    namespace :v1 do
+      resources :appointments
       resources :service_providers
+    end
+  end
 end
